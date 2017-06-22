1c321c62a62e4789b8448857c7bd0b29b703b421
Dell <Dell@SHREYAS>
2017-06-22 15:07:51 +0530

    Again updated..

--- PlanetSportsFrontend/src/main/java/com/niit/PlanetSportsFrontend/controller/PageController.java
+++ PlanetSportsFrontend/src/main/java/com/niit/PlanetSportsFrontend/controller/PageController.java
@@ -2,10 +2,8 @@ package com.niit.PlanetSportsFrontend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Controller
-@EnableWebMvc
 public class PageController 
 {
    
