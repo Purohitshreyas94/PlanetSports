2930a3f845bb72d01d1ec1b6c69c76f084a9fa19
Dell <Dell@SHREYAS>
2017-06-22 12:09:16 +0530

    changed again

--- PlanetSportsFrontend/src/main/java/com/niit/PlanetSportsFrontend/controller/PageController.java
+++ PlanetSportsFrontend/src/main/java/com/niit/PlanetSportsFrontend/controller/PageController.java
@@ -2,8 +2,10 @@ package com.niit.PlanetSportsFrontend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Controller
+@EnableWebMvc
 public class PageController 
 {
    
